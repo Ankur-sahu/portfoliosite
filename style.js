06ebe380e985263cef604bbe2dc16abdc8303325
Ankur-sahu <117460405+Ankur-sahu@users.noreply.github.com>
2022-11-24 00:41:14 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,11 +1,10 @@
 let star = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
-let text = document.getElementById('text');
 let btn = document.getElementById('btn');
-let mountains_front = document.getElementById('mountains_front');
 //let header = document.querySelector('header');
 let info = document.getElementById('info');
+let rect = info.getBoundingClientRect();//getting X and Y position of element
 let developer = document.getElementById('developer');
 
 window.addEventListener('scroll',()=>{
@@ -13,7 +12,6 @@ window.addEventListener('scroll',()=>{
     star.style.left = value * 0.25 + "px";
     moon.style.top = value *1.3 + "px";
     mountains_behind.style.top = value *0.5 + "px";
-    info.style.top = 240 + value + "px";
-    //text.style.bottom = 200 + -value + "px";
+    info.style.top = rect.y + value + "px";
     btn.style.marginTop = value *1.5 + "px";
 }) 
